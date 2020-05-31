0350e2f46291072627f3c61e5c5fa31eb7b8f62d
ramy1415 <ramy1415@hotmail.com>
2020-05-31 22:16:27 +0200

    create items model

--- todo_list_app/models.py
+++ todo_list_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Item(models.Model):
+
+    class Status(models.TextChoices):
+        ACTIVE = 'A', _('Active')
+        DONE = 'D', _('Done')
+
+
+    description = models.CharField(max_length=200)
+    status = models.CharField(
+        max_length=1,
+        choices=Status.choices,
+        default=Status.ACTIVE,
+    )
