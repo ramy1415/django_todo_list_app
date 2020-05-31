e307ebb0c94150f200d93df159e11e2c19a2503f
ramy1415 <ramy1415@hotmail.com>
2020-05-31 21:36:12 +0200

    adding static files and templates folder and media url

--- DjangoTask/settings.py
+++ DjangoTask/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#setting website media folder and url
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -54,7 +57,8 @@ ROOT_URLCONF = 'DjangoTask.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # setting templates folder
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+#adding static directories
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "media"),
+]
